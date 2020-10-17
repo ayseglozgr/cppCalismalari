325d4c49d9853b654d7e264768c6e273fb65fdba
ayseglozgr <70823020+ayseglozgr@users.noreply.github.com>
2020-10-18 00:53:04 +0300

    Add files via upload

---
+++ ustelOrnek.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int a;
+	for (a = 1; a <= 20; a++) {
+	cout << pow(2, a) - 1 << endl;
+	
+}
+	int count (a);
+	{
+		cout << a << endl;
+
+	}
+
+	return 0;
+}
