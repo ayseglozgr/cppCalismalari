8ed443a071643eca6d53ec01c583f17588a3f83f
ayseglozgr <70823020+ayseglozgr@users.noreply.github.com>
2020-10-18 03:16:28 +0300

    Add files via upload

---
+++ aritmatikOrtalama.cpp
@@ -0,0 +1,21 @@
+#include< iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, e, f, g, h, i, j;
+	cout << " enter a number : " << endl;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> d;
+	cin >> e;
+	cin >> f;
+	cin >> g;
+	cin >> h;
+	cin >> i;
+	cin >> j;
+	cout << (a + b + c + d + e + f + g + h + i + j)/10;
+
+
+}
