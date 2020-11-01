09e8de257a58d8faf14d396cc500d54d4687d8c0
ayseglozgr <70823020+ayseglozgr@users.noreply.github.com>
2020-11-01 11:52:55 +0300

    asalMiFunc
    girilen bir sayinin asal olup olmadıgını kontrol eder.

--- asalMiFunc.cpp
+++ asalMiFunc.cpp
@@ -4,30 +4,22 @@ using namespace std;
 bool asalMi(int a)
 {
 	int k;
-	for (k = 2; k <= a/2; k++)
-	{
-		if (a % k == 0)
-		{
+	for (k = 2; k <= a/2; k++){
+		if (a % k == 0){
 			return false;
-		}
-		else
-		{
+		}else{
 			return true;
 		}
 	}
 }
-int main()
-{
+int main(){
 	int y;
 	cout << " enter a number : " << endl;
 	cin >> y;
-	if (asalMi(y) == true)
-	{
+	if (asalMi(y) == true){
 		cout << " asal " << endl;
-	}
-	else
-	{
-		cout << " asal degil " << endl;
+	}else{
+	cout << " asal degil " << endl;
 	}
 	}
 
