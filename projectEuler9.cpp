1d9078a9c212028abb3eded77c7a4b4955e229f6
ayseglozgr <70823020+ayseglozgr@users.noreply.github.com>
2020-12-10 10:17:00 +0300

    Add files via upload
    A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a^2 + b^2 = c^2. For example, 32 + 42 = 9 + 16 = 25 = 52. There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.

---
+++ projectEuler9.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	int j;
+	int k;
+	int temp;
+	for (i = 1; i < 1000; i++)
+	{
+		for (j = 1; j < 1000; j++)
+		{
+			for (k = 1; k < 1000; k++)
+			{
+				temp = i * i + j * j;
+				if (temp==k * k && i + j + k==1000)
+				{
+					if (i<j)
+					{
+						cout << "a : " << i << endl;
+						cout << "b : " << j << endl;
+						cout << "c : " << k << endl;
+						cout << i << j << k << endl;
+					}
+				}
+			}
+		}
+		
+	}
+	system("pause");
+}
